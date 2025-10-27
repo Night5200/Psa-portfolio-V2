d4d962405036e3f365dd9e97dcfd9a5b3d0ad24a
Night5200 <126689654+Night5200@users.noreply.github.com>
2025-10-27 16:42:00 +0530

    Update gumlet-config.tsx

--- lib/gumlet-config.tsx
+++ lib/gumlet-config.tsx
@@ -74,7 +74,7 @@ const OUR_WORKS_EMBEDS = [
             style="border:none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;"
           allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture; fullscreen;">
           </iframe>
-    </div>,
+    </div>' ,
   `<div style="position:relative;aspect-ratio:16/9;"><iframe loading="lazy" title="Gumlet video player" src="https://play.gumlet.io/embed/YOUR_ID_4?background=false&autoplay=false&loop=false&disableControls=false" style="border:none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture; fullscreen;"></iframe></div>`,
 ]
 
